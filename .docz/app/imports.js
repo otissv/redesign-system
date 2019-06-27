6d7852cc8a74bf756e04c1872704b725887a3f39
Otis Virginie <otissv@gmail.com>
2019-06-28 00:10:54 +0100

    docz build

---
+++ .docz/app/imports.js
@@ -0,0 +1,134 @@
+export const imports = {
+  'src/Accordion/accordion.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-accordion-accordion" */ 'src/Accordion/accordion.mdx'
+    ),
+  'src/Alert/alert.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-alert-alert" */ 'src/Alert/alert.mdx'
+    ),
+  'src/AppBar/appBar.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-app-bar-app-bar" */ 'src/AppBar/appBar.mdx'
+    ),
+  'src/Badge/badge.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-badge-badge" */ 'src/Badge/badge.mdx'
+    ),
+  'src/Button/button.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-button-button" */ 'src/Button/button.mdx'
+    ),
+  'src/ButtonGroup/buttonGroup.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-button-group-button-group" */ 'src/ButtonGroup/buttonGroup.mdx'
+    ),
+  'src/ButtonIcon/buttonIcon.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-button-icon-button-icon" */ 'src/ButtonIcon/buttonIcon.mdx'
+    ),
+  'src/Card/card.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-card-card" */ 'src/Card/card.mdx'
+    ),
+  'src/Container/container.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-container-container" */ 'src/Container/container.mdx'
+    ),
+  'src/CoverImg/coverImg.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-cover-img-cover-img" */ 'src/CoverImg/coverImg.mdx'
+    ),
+  'src/Dropdown/dropdown.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-dropdown-dropdown" */ 'src/Dropdown/dropdown.mdx'
+    ),
+  'src/ErrorMessage/ErrorMessage.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-error-message-error-message" */ 'src/ErrorMessage/ErrorMessage.mdx'
+    ),
+  'src/Flex/flex.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-flex-flex" */ 'src/Flex/flex.mdx'
+    ),
+  'src/Form/form.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-form-form" */ 'src/Form/form.mdx'
+    ),
+  'src/GoolgeMaps/GoogleMaps.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-goolge-maps-google-maps" */ 'src/GoolgeMaps/GoogleMaps.mdx'
+    ),
+  'src/Icon/Icons.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-icon-icons" */ 'src/Icon/Icons.mdx'
+    ),
+  'src/Img/img.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-img-img" */ 'src/Img/img.mdx'
+    ),
+  'src/Input/input.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-input-input" */ 'src/Input/input.mdx'
+    ),
+  'src/Portal/portal.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-portal-portal" */ 'src/Portal/portal.mdx'
+    ),
+  'src/Table/table.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-table-table" */ 'src/Table/table.mdx'
+    ),
+  'src/Tabs/tabs.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-tabs-tabs" */ 'src/Tabs/tabs.mdx'
+    ),
+  'src/Textarea/textarea.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-textarea-textarea" */ 'src/Textarea/textarea.mdx'
+    ),
+  'src/Toast/toast.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-toast-toast" */ 'src/Toast/toast.mdx'
+    ),
+  'src/Toggle/toogle.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-toggle-toogle" */ 'src/Toggle/toogle.mdx'
+    ),
+  'src/Toolbar/toolbar.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-toolbar-toolbar" */ 'src/Toolbar/toolbar.mdx'
+    ),
+  'src/Typography/typography.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-typography-typography" */ 'src/Typography/typography.mdx'
+    ),
+  'src/Video/video.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-video-video" */ 'src/Video/video.mdx'
+    ),
+  'src/theme/border/border.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-theme-border-border" */ 'src/theme/border/border.mdx'
+    ),
+  'src/theme/color/colors.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-theme-color-colors" */ 'src/theme/color/colors.mdx'
+    ),
+  'src/theme/elevate/elevate.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-theme-elevate-elevate" */ 'src/theme/elevate/elevate.mdx'
+    ),
+  'src/theme/font/font.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-theme-font-font" */ 'src/theme/font/font.mdx'
+    ),
+  'src/theme/radius/radius.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-theme-radius-radius" */ 'src/theme/radius/radius.mdx'
+    ),
+  'src/theme/unit/unit.mdx': () =>
+    import(
+      /* webpackPrefetch: true, webpackChunkName: "src-theme-unit-unit" */ 'src/theme/unit/unit.mdx'
+    ),
+}
